5f9cb30b76c149706f03db5c909e74ddfeff9cee
poppabear8883 <servnx@gmail.com>
2018-01-13 22:38:57 -0500

    feat(api): Add country relationship to fan model

--- app/Fan.php
+++ app/Fan.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fan extends Model
 {
+    public function country()
+    {
+        return $this->hasOne(Country::class);
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);
