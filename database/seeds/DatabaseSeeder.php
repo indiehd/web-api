5076ec17b9e6573178decf377e5cfba15c68ea86
poppabear8883 <servnx@gmail.com>
2018-01-14 18:52:36 -0500

    build(database): Add basic account seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,6 +4,17 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+
+    private $types = [
+        App\CatalogEntity::class,
+        App\Fan::class
+    ];
+
+    private $catalogable = [
+        App\Artist::class,
+        App\Label::class
+    ];
+
     /**
      * Run the database seeds.
      *
@@ -11,6 +22,39 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->seedAccounts(10);
+    }
+
+    private function seedAccounts($amount = 5)
+    {
+        for ($i = 1; $i <= $amount; $i++) {
+            $catalogable_type = $this->catalogable[rand(0, count($this->catalogable)-1)];
+
+            $type = app($this->types[rand(0, count($this->types)-1)]);
+
+            if ($type instanceof App\CatalogEntity) {
+
+                $this->log('Creating A CatalogEntity');
+                $this->log("CatalogEntity Type is: $catalogable_type");
+
+                $type::create([
+                    'user_id' => factory(App\User::class)->create()->id,
+                    'catalogable_id' => factory($catalogable_type)->create()->id,
+                    'catalogable_type' => $catalogable_type
+                ]);
+            } else {
+
+                $this->log('Creating A Fan');
+
+                $type::create([
+                    'user_id' => factory(App\User::class)->create()->id
+                ]);
+            }
+        }
+    }
+
+    private function log($msg)
+    {
+        return $this->command->getOutput()->writeln("<info>LOG:</info> $msg");
     }
 }
