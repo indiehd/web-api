a19273a146ff93b66d45834a73f48c6f53145a8d
poppabear8883 <servnx@gmail.com>
2018-01-15 11:02:27 -0500

    feat(api): Add validation to user api controller

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Contracts\UserRepositoryInterface;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Contracts\Validation\Factory as ValidatorInterface;
 
 class UserController extends Controller
 {
@@ -15,33 +16,61 @@ class UserController extends Controller
      */
     private $user;
 
-    public function __construct(UserRepositoryInterface $user)
+    /**
+     * @var ValidatorInterface
+     */
+    private $validator;
+
+    public function __construct(UserRepositoryInterface $user, ValidatorInterface $validator)
     {
         $this->user = $user;
+        $this->validator = $validator;
     }
 
     public function all()
     {
-        return $this->user->all();
+        return response($this->user->all(), 200);
     }
 
     public function getById($id)
     {
-        return $this->user->findById($id);
+        return response($this->user->findById($id), 200);
     }
 
     public function create(Request $request)
     {
-        return $this->user->create($request->all());
+        $this->check($request->all(), [
+            'email' => 'required|unique:users',
+            'password' => 'required',
+        ]);
+
+        return response($this->user->create($request->all()), 200);
     }
 
     public function update(Request $request, $id)
     {
-        return $this->user->update($id, $request->all());
+        return response($this->user->update($id, $request->all()), 200);
     }
 
     public function delete($id)
     {
-        return $this->user->delete($id);
+        return response($this->user->delete($id), 200);
+    }
+
+    /*
+     * ------------------
+     * Protected Methods
+     * ------------------
+     */
+
+    protected function check(array $data, array $rules)
+    {
+        $validation = $this->validator->make($data, $rules);
+
+        if($validation->fails()) {
+            return response($validation->errors()->toArray(), 400);
+        }
+
+        return true;
     }
 }
