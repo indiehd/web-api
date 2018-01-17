c0c368dcdc4bce181f388a2b35a23a998420eefe
poppabear8883 <servnx@gmail.com>
2018-01-17 10:52:08 -0500

    feat(database): Add required fields for 'fans' migration

--- database/migrations/2018_01_13_184708_create_fans_table.php
+++ database/migrations/2018_01_13_184708_create_fans_table.php
@@ -15,10 +15,19 @@ class CreateFansTable extends Migration
     {
         Schema::create('fans', function (Blueprint $table) {
             $table->increments('id');
-
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('address_one');
+            $table->string('address_two')->nullable();
+            $table->string('city');
+            $table->string('territory');
+            $table->char('country_code', 2);
+            $table->string('postal_code');
+            $table->string('phone');
+            $table->string('alt_phone')->nullable();
             $table->unsignedInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
-
             $table->timestamps();
         });
     }
