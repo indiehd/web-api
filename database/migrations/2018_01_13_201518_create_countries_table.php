3654c972c1ae09f22f12647c163fef2f0d2376ac
poppabear8883 <servnx@gmail.com>
2018-01-16 23:58:59 -0500

    feat(database): Add required fields to countries migration

--- database/migrations/2018_01_13_201518_create_countries_table.php
+++ database/migrations/2018_01_13_201518_create_countries_table.php
@@ -15,6 +15,8 @@ class CreateCountriesTable extends Migration
     {
         Schema::create('countries', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('code', 2);
+            $table->string('name');
             $table->timestamps();
         });
     }
