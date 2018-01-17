3e38c0903a22d34b0a6238b20aa2cf259a445e53
poppabear8883 <servnx@gmail.com>
2018-01-17 00:03:13 -0500

    feat(api): Add user api resource

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\Resource;
+
+class UserResource extends Resource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'username' => $this->username,
+            'last_login' => $this->last_login,
+            'profile' => $this->profile(),
+            'created_at' => $this->created_at
+        ];
+    }
+
+    private function profile()
+    {
+        // todo: Add a FanResource -mike
+        return $this->fan ? $this->fan : new CatalogResource($this->entity);
+    }
+}
