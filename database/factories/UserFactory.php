ab0f9728d72469ac038f1b3fcbe8e3589d77f9b1
poppabear8883 <servnx@gmail.com>
2018-01-16 19:15:46 -0500

    fix: Update User factory to use the correct fields

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,8 +15,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\User::class, function (Faker $faker) {
     return [
-        'email' => $faker->unique()->safeEmail,
-        'password' => bcrypt('secret'),
-        'remember_token' => str_random(10),
+        'username' => $faker->unique()->userName,
+        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
     ];
 });
