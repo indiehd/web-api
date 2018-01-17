a4c421a84108cda3f07f3def46532c9aa338ad82
poppabear8883 <servnx@gmail.com>
2018-01-16 23:58:20 -0500

    feat(database): Add required fields to catalog_entities migration

--- database/migrations/2018_01_13_184726_create_catalog_entities_table.php
+++ database/migrations/2018_01_13_184726_create_catalog_entities_table.php
@@ -15,13 +15,25 @@ class CreateCatalogEntitiesTable extends Migration
     {
         Schema::create('catalog_entities', function (Blueprint $table) {
             $table->increments('id');
-
+            $table->string('moniker');
+            $table->string('alt_moniker')->nullable();
+            $table->string('city');
+            $table->string('territory');
+            $table->char('country_code', 2);
+            $table->foreign('country_code')->references('code')->on('countries');
+            $table->string('official_url')->nullable();
+            $table->string('profile_url');
+            $table->unsignedInteger('rank')->default(0);
+            $table->boolean('is_active')->default(false);
+            $table->timestamp('approved_at')->nullable();
             $table->unsignedInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
-
+            $table->unsignedInteger('approver_id')->nullable();
+            $table->foreign('approver_id')->references('id')->on('users');
+            $table->unsignedInteger('deleter_id')->nullable();
+            $table->foreign('deleter_id')->references('id')->on('users');
             $table->unsignedInteger('catalogable_id');
             $table->string('catalogable_type');
-
             $table->timestamps();
         });
     }
