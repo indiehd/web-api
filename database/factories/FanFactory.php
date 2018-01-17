26c66d7a225bdb839d7ddd7b229ba731d3b6371e
poppabear8883 <servnx@gmail.com>
2018-01-17 10:47:45 -0500

    feat(database): Add required faker data for Fan Entities

--- database/factories/FanFactory.php
+++ database/factories/FanFactory.php
@@ -1,9 +1,31 @@
 <?php
 
 use Faker\Generator as Faker;
+use Faker\Provider\en_US\Address;
+use Faker\Provider\en_US\Person;
+use Faker\Provider\en_US\PhoneNumber;
+use Faker\Provider\en_US\Company;
 
 $factory->define(App\Fan::class, function (Faker $faker) {
+    $faker->addProvider(new Person($faker));
+    $faker->addProvider(new Address($faker));
+    $faker->addProvider(new PhoneNumber($faker));
+    $faker->addProvider(new Company($faker));
+
     return [
-        //
+        'first_name' => $faker->firstName,
+        'last_name' => $faker->lastName,
+        'email' => $faker->email,
+        'address_one' => $faker->streetAddress,
+        'address_two' => $faker->boolean(25) ? $faker->secondaryAddress : null,
+        'city' => $faker->city,
+        'territory' => $faker->state,
+        'country_code' => 'US',
+        'postal_code' => $faker->postcode,
+        'phone' => $faker->phoneNumber,
+        'alt_phone' => $faker->boolean(25) ? $faker->phoneNumber : null,
+        'user_id' => function () {
+            return factory(App\User::class)->create()->id;
+        }
     ];
 });
