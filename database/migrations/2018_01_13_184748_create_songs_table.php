6aa6aa505467d1650c1aee357c6f01ab8cdc2d46
poppabear8883 <servnx@gmail.com>
2018-01-22 12:49:29 -0500

    feat: Add fields to songs table migration

--- database/migrations/2018_01_13_184748_create_songs_table.php
+++ database/migrations/2018_01_13_184748_create_songs_table.php
@@ -15,6 +15,20 @@ class CreateSongsTable extends Migration
     {
         Schema::create('songs', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->string('alt_name')->nullable();
+            $table->unsignedTinyInteger('track_number')->default(1);
+            $table->float('preview_start')->default(0.00);
+            $table->boolean('is_active')->default(true);
+            //$table->boolean('is_in_back_catalog')->default(false);
+            //$table->unsignedInteger('catalog_id');
+            //$table->foreign('catalog_id')->references('id')->on('catalogs');
+            $table->unsignedInteger('sku_id');
+            $table->foreign('sku_id')->references('id')->on('skus');
+            $table->unsignedInteger('album_id');
+            $table->foreign('album_id')->references('id')->on('albums');
+            $table->unique(['album_id', 'track_number']);
+            $table->softDeletes();
             $table->timestamps();
         });
     }
