271628ac72ad5741e62a1f43942b20c223fa0eff
Ben Johnson <bjohnson@indiehd.com>
2018-08-10 23:07:35 -0400

    fix: Modify relationship to reflect that a Song belongs to *one* Album

--- app/Song.php
+++ app/Song.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Song extends Model
 {
-    public function albums()
+    public function album()
     {
         return $this->belongsTo(Album::class);
     }
