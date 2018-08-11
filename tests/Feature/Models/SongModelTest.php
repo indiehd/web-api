6e0a386051408a235df06885c6a6c245eb5de637
Ben Johnson <ben@indiehd.com>
2018-08-11 17:21:01 -0400

    tests: Add tests for FlacFile and Sku relationships

--- tests/Feature/Models/SongModelTest.php
+++ tests/Feature/Models/SongModelTest.php
@@ -8,6 +8,8 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use AlbumsSeeder;
 use App\Song;
 use App\Album;
+use App\FlacFile;
+use App\Sku;
 
 class SongModelTest extends TestCase
 {
@@ -28,5 +30,20 @@ class SongModelTest extends TestCase
     public function test_albums_randomSong_belongsToAlbum()
     {
         $this->assertInstanceOf(Album::class, Song::inRandomOrder()->first()->album);
+    }
+
+    /**
+     * Ensure that any random Song has one FlacFile.
+     *
+     * @return true
+     */
+    public function test_flacFile_randomSong_hasOneFlacFile()
+    {
+        $this->assertInstanceOf(FlacFile::class, Song::inRandomOrder()->first()->flacFile);
+    }
+
+    public function test_sku_randomSong_hasOneSku()
+    {
+        $this->assertInstanceOf(Sku::class, Song::inRandomOrder()->first()->sku);
     }
 }
