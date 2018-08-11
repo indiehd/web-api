d1cdde10446468bb83b51560400778fe336f070b
Ben Johnson <ben@indiehd.com>
2018-08-11 13:49:10 -0400

    fix: Move tests from Unit into Feature (they require DB)

--- tests/Feature/Models/AlbumModelTest.php
+++ tests/Feature/Models/AlbumModelTest.php
@@ -5,10 +5,20 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use AlbumsSeeder;
 use App\Album;
 
 class AlbumModelTest extends TestCase
 {
+    use RefreshDatabase;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->seed(AlbumsSeeder::class);
+    }
+
     /**
      * Ensure that any random Album has one or more Songs.
      *
--- tests/Feature/Models/SongModelTest.php
+++ tests/Feature/Models/SongModelTest.php
@@ -5,11 +5,21 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use AlbumsSeeder;
 use App\Song;
 use App\Album;
 
 class SongModelTest extends TestCase
 {
+    use RefreshDatabase;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->seed(AlbumsSeeder::class);
+    }
+
     /**
      * Ensure that any random Song belongs to an Album.
      *
