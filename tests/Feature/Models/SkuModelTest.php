c227be2b4c7562a7dcf9256796cec031ef972a1e
Ben Johnson <ben@indiehd.com>
2018-08-14 10:36:10 -0400

    tests: Add tests to cover Sku model

---
+++ tests/Feature/Models/SkuModelTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature\Models;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use DatabaseSeeder;
+use App\Sku;
+use App\Song;
+
+class SongModelTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->seed(DatabaseSeeder::class);
+    }
+
+    /**
+     * Ensure that any random Sku has one or more Songs.
+     *
+     * @return void
+     */
+    public function test_song_randomSku_hasManySongs()
+    {
+        $this->assertInstanceOf(Song::class, Sku::inRandomOrder()->first()->songs->first());
+    }
+}
