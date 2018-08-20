733f6ca36fcd60930bce43ebc36298582559700d
Ben Johnson <ben@indiehd.com>
2018-08-19 22:08:00 -0400

    api: Add Album repository
    This is merely in preparation to begin writing tests and the implementation.

---
+++ app/Repositories/AlbumRepository.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Contracts\AlbumRepositoryInterface;
+
+class AlbumRepository extends BaseRepository implements AlbumRepositoryInterface
+{
+    /**
+     * @var string $class
+     */
+    protected $class = Album::class;
+
+    /**
+     * @var Album
+     */
+    protected $album;
+
+    public function __construct(Album $album)
+    {
+        $this->album = $album;
+    }
+
+    public function class()
+    {
+        return $this->class;
+    }
+
+    public function model()
+    {
+        return $this->album;
+    }
+}
