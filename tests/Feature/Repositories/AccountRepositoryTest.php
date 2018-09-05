dd561bc63b7d1a0843f509dac66c065023324015
Ben Johnson <ben@indiehd.com>
2018-09-05 17:00:36 -0400

    tests: Add test for Account repository

---
+++ tests/Feature/Repositories/AccountRepositoryTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Tests\Feature\Repositories;
+
+use CountriesSeeder;
+use App\Contracts\AccountRepositoryInterface;
+use App\Contracts\UserRepositoryInterface;
+
+class AccountRepositoryTest extends RepositoryCrudTestCase
+{
+    /**
+     * @var $user UserRepositoryInterface
+     */
+    protected $user;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->seed(CountriesSeeder::class);
+
+        $this->user = resolve(UserRepositoryInterface::class);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setRepository()
+    {
+        $this->repo = resolve(AccountRepositoryInterface::class);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function test_method_create_storesNewModel()
+    {
+        $account = factory($this->repo->class())->make([
+            'user_id' => factory($this->user->class())->create()->id
+        ])->toArray();
+
+        $this->assertInstanceOf(
+            $this->repo->class(),
+            $this->repo->create($account)
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function test_method_update_updatesModel()
+    {
+        $account = factory($this->repo->class())->create([
+            'user_id' => factory($this->user->class())->create()->id
+        ]);
+
+        $newValue = 'Foobius Barius';
+
+        $this->repo->update($account->id, [
+            'first_name' => $newValue,
+        ]);
+
+        $this->assertTrue(
+            $this->repo->findById($account->id)->first_name === $newValue
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function test_method_delete_deletesModel()
+    {
+        $account = factory($this->repo->class())->make([
+            'user_id' => factory($this->user->class())->create()->id
+        ]);
+
+        $account->delete();
+
+        $this->assertNull($this->repo->findById($account->id));
+    }
+}
