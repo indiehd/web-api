419a6a72ebaabdf3ae1f0b5a046698213c46f02c
poppabear8883 <servnx@gmail.com>
2018-09-14 20:24:30 -0400

    api(Resources): Update Account resource to use UserResource

--- app/Http/Resources/AccountResource.php
+++ app/Http/Resources/AccountResource.php
@@ -15,7 +15,6 @@ class AccountResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'user_id' => $this->user_id,
             'email' => $this->email,
             'first_name' => $this->first_name,
             'last_name' => $this->last_name,
@@ -28,6 +27,7 @@ class AccountResource extends JsonResource
             'postal_code' => $this->postal_code,
             'phone' => $this->phone,
             'alt_phone' => $this->alt_phone,
+            'user' => new UserResource($this->user),
         ];
     }
 }
