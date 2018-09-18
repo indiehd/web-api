e63aba5f93cea602fc3a2019784d9d4c53b8947f
Ben Johnson <ben@indiehd.com>
2018-09-17 20:14:13 -0400

    tests: Move Eloquent relationship tests into Repository test class
    Also, remove transaction around deletion test; the transaction is
    implemented in the BaseRepository's delete() method now.

--- tests/Feature/Repositories/SkuRepositoryTest.php
+++ tests/Feature/Repositories/SkuRepositoryTest.php
@@ -4,10 +4,45 @@ namespace Tests\Feature\Repositories;
 
 use DB;
 
+use CountriesSeeder;
 use App\Contracts\SkuRepositoryInterface;
+use App\Contracts\ArtistRepositoryInterface;
+use App\Contracts\AlbumRepositoryInterface;
+use App\Contracts\SongRepositoryInterface;
 
 class SkuRepositoryTest extends RepositoryCrudTestCase
 {
+    /**
+     * @var ArtistRepositoryInterface $artist
+     */
+    protected $artist;
+
+    /**
+     * @var AlbumRepositoryInterface $album
+     */
+    protected $album;
+
+    /**
+     * @var SongRepositoryInterface $song
+     */
+    protected $song;
+
+    /**
+     * @inheritdoc
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->seed(CountriesSeeder::class);
+
+        $this->artist = resolve(ArtistRepositoryInterface::class);
+
+        $this->album = resolve(AlbumRepositoryInterface::class);
+
+        $this->song = resolve(SongRepositoryInterface::class);
+    }
+
     /**
      * @inheritdoc
      */
@@ -68,10 +103,31 @@ class SkuRepositoryTest extends RepositoryCrudTestCase
     {
         $sku = factory($this->repo->class())->create();
 
-        DB::transaction(function () use ($sku) {
-            $sku->delete();
-        });
+        $sku->delete();
 
         $this->assertNull($this->repo->findById($sku->id));
+    }
+
+    /**
+     * Ensure that when a Song is associated with a Sku, the Sku has many Songs.
+     *
+     * @return void
+     */
+    public function test_song_newSku_hasManySongs()
+    {
+        $album = factory($this->album->class())->create([
+            'artist_id' => factory($this->artist->class())->create()->id
+        ]);
+
+        $song = factory($this->song->class())->create([
+            'track_number' => 1,
+            'album_id' => $album->id,
+        ]);
+
+        $sku = factory($this->repo->class())->create();
+
+        $song->sku()->associate($sku)->save();
+
+        $this->assertInstanceOf($this->song->class(), $sku->songs->first());
     }
 }
