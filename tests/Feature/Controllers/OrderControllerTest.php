d9f5b78e7cd1c173cb5447503ccbc10674ac30c1
Ben Johnson <ben@indiehd.com>
2018-12-29 22:53:15 -0500

    tests: Round-out test logic and add proper docblocks

--- tests/Feature/Controllers/OrderControllerTest.php
+++ tests/Feature/Controllers/OrderControllerTest.php
@@ -3,11 +3,30 @@
 namespace Tests\Feature\Controllers;
 
 use CountriesSeeder;
-
 use App\Contracts\OrderRepositoryInterface;
+use App\Contracts\UserRepositoryInterface;
+use App\Contracts\AccountRepositoryInterface;
 
 class OrderControllerTest extends ControllerTestCase
 {
+    /**
+     * @var $order OrderRepositoryInterface
+     */
+    protected $order;
+
+    /**
+     * @var $user UserRepositoryInterface
+     */
+    protected $user;
+
+    /**
+     * @var $account AccountRepositoryInterface
+     */
+    protected $account;
+
+    /**
+     * @inheritdoc
+     */
     public function setUp()
     {
         parent::setUp();
@@ -15,8 +34,17 @@ class OrderControllerTest extends ControllerTestCase
         $this->seed(CountriesSeeder::class);
 
         $this->order = resolve(OrderRepositoryInterface::class);
+
+        $this->user = resolve(UserRepositoryInterface::class);
+
+        $this->account = resolve(AccountRepositoryInterface::class);
     }
 
+    /**
+     * Define the JSON structure that is expected for the most common responses.
+     *
+     * @return array
+     */
     public function getJsonStructure()
     {
         return [
@@ -24,28 +52,21 @@ class OrderControllerTest extends ControllerTestCase
         ];
     }
 
-    public function getAllAccountInputsInValidState()
-    {
-        return [
-            'email' => 'foobar@example.com',
-            'first_name' => 'Foobius',
-            'last_name' => 'Barius',
-            'address_one' => '123 Any Street',
-            'address_two' => 'Apt 1',
-            'city' => 'New York',
-            'territory' => 'New York',
-            'country_code' => 'US',
-            'postal_code' => '10110',
-            'phone' => '+1 510 200 3000',
-            'alt_phone' => null,
-        ];
-    }
-
+    /**
+     * Generate an exhaustive list of valid inputs for use with test methods
+     * that accept inputs.
+     *
+     * @return array
+     */
     public function getAllInputsInValidState()
     {
         return [];
     }
 
+    /**
+     * Ensure that a request for the index returns OK HTTP status and the
+     * expected JSON structure.
+     */
     public function testAllReturnsOkStatusAndExpectedJsonStructure()
     {
         $this->order->create([]);
@@ -59,6 +80,10 @@ class OrderControllerTest extends ControllerTestCase
             ]);
     }
 
+    /**
+     * Ensure that a request for an existing record returns OK HTTP status and
+     * the expected JSON structure.
+     */
     public function testShowReturnsOkStatusAndExpectedJsonStructure()
     {
         $order = $this->order->create([]);
@@ -70,6 +95,10 @@ class OrderControllerTest extends ControllerTestCase
             ]);
     }
 
+    /**
+     * Ensure that requests with valid input result in OK HTTP status and the
+     * expected JSON structure.
+     */
     public function testStoreWithValidInputReturnsOkStatusAndExpectedJsonStructure()
     {
         $this->json('POST', route('orders.store'), $this->getAllInputsInValidState())
@@ -79,61 +108,81 @@ class OrderControllerTest extends ControllerTestCase
             ]);
     }
 
-    public function testStoreWithInvalidInputReturnsUnprocessableEntityStatusAndExpectedJsonStructure()
+    /**
+     * Ensure that any invalid input is ignored and a success response returned
+     * in such cases.
+     */
+    public function testStoreWithInvalidInputReturnsOkStatusAndExpectedJsonStructure()
     {
-        $this->json('POST', route('orders.store'), [])
-            ->assertStatus(422)
-            ->assertJsonStructure([
-                'message',
-                'errors'
-            ]);
-    }
-
-    public function testUpdateWithValidInputReturnsOkStatusAndExpectedJsonStructure()
-    {
-        $user = $this->createUser();
-
-        $inputs = $this->getAllInputsInValidState();
-
-        $inputs['username'] = 'FoobiusBazius';
-
-        $this->json('PUT', route('orders.update', ['id' => $user->id]), $inputs)
-            ->assertStatus(200)
+        $this->json('POST', route('orders.store'), [
+            'unexpected' => 'junk',
+        ])
+            ->assertStatus(201)
             ->assertJsonStructure([
                 'data' => $this->getJsonStructure()
             ]);
     }
 
-    public function testUpdateWithInvalidInputReturnsUnprocessableEntityStatusAndExpectedJsonStructure()
+    /**
+     * Ensure that any attempt to update an Order fails with an Unauthorized
+     * response, as Orders should be immutable once created.
+     */
+    public function testUpdateReturnsUnauthorizedStatus()
     {
-        $user = $this->createUser();
+        $order = $this->order->create([]);
 
-        $this->json('PUT', route('orders.update', ['id' => $user->id]), ['email' => ''])
-            ->assertStatus(422)
-            ->assertJsonStructure([
-                'message',
-                'errors'
-            ]);
+        $this->json('PUT', route('orders.update', ['id' => $order->id]))
+            ->assertStatus(403);
     }
 
+    /**
+     * Ensure that when a valid ID is supplied, the record is destroyed, and an
+     * OK HTTP status is returned, along with the expected JSON structure.
+     */
     public function testDestroyWithValidInputReturnsOkStatusAndExpectedJsonStructure()
     {
-        $user = $this->createUser();
+        $order = factory($this->order->class())->create();
 
-        $this->json('DELETE', route('orders.destroy', ['id' => $user->id]))
+        $this->json('DELETE', route('orders.destroy', ['id' => $order->id]))
             ->assertStatus(200)
             ->assertJsonStructure([]);
     }
 
+    /**
+     * Ensure that when a invalid ID is supplied, an Unprocessable Entity HTTP
+     * status code is returned.
+     */
     public function testDestroyWithInvalidInputReturnsUnprocessableEntityStatus()
     {
         $this->json('DELETE', route('orders.destroy', ['id' => 'foo']))
             ->assertStatus(404);
     }
 
+    /**
+     * Ensure that when no ID is supplied, a Method Not Allowed HTTP status code
+     * is returned.
+     */
     public function testDestroyWithMissingInputReturnsMethodNotAllowedStatus()
     {
         $this->json('DELETE', route('orders.destroy', ['id' => null]))
             ->assertStatus(405);
+    }
+
+    /**
+     * Create a User.
+     *
+     * @return \App\User
+     */
+    protected function createUser()
+    {
+        $user = factory($this->user->class())->make();
+
+        $user = $this->user->create([
+            'email' => $user->email,
+            'password' => $user->password,
+            'account' => factory($this->account->class())->raw()
+        ]);
+
+        return $user;
     }
 }
