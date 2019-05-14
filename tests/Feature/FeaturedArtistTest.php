ace41d7c9a7f47a4c94ba81b006d91cb42de456e
Ben Johnson <ben@indiehd.com>
2019-05-14 17:03:59 -0400

    tests: Refine requirements

--- tests/Feature/FeaturedArtistTest.php
+++ tests/Feature/FeaturedArtistTest.php
@@ -5,6 +5,8 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
+use Carbon\Carbon;
+
 use App\Contracts\FeaturedRepositoryInterface;
 use App\Contracts\AlbumRepositoryInterface;
 use App\Contracts\ArtistRepositoryInterface;
@@ -50,38 +52,125 @@ class FeaturedArtistTest extends TestCase
         $this->artist = resolve(ArtistRepositoryInterface::class);
 
         $this->profile = resolve(ProfileRepositoryInterface::class);
-    }
 
-    public function testWhenArtistIsFeaturedItHasAccountProfile()
-    {
-        $album = $this->makeAlbum();
+        // Two Artists that DO NOT meet every condition...
 
-        factory($this->featured->class())->create([
-            'featurable_id' => $album->artist->id,
+        // ... Artist doesn't have a Profile.
+
+        $artist = $this->createArtistAndDeleteItsProfile();
+
+        // ... Artist doesn't have at least one Album.
+
+        $this->createArtist();
+
+        // Three Artists that DO meet every condition.
+
+        $artist = $this->createArtist();
+
+        $this->makeAlbum(['artist_id' => $artist->id])->save();
+
+        $artist = $this->createArtist();
+
+        $this->makeAlbum(['artist_id' => $artist->id])->save();
+
+        $artist = $this->createArtist();
+
+        $this->makeAlbum(['artist_id' => $artist->id])->save();
+
+        // Two more Artists...
+
+        // ... Artist is currently featured (and has been for more than 7 days
+        // but fewer than 180 days), which makes the Artist ineligible.
+
+        $artist = $this->createArtist();
+
+        $this->makeAlbum(['artist_id' => $artist->id])->save();
+
+        $featured = $this->featured->create([
+            'featurable_id' => $artist->id,
             'featurable_type' => $this->artist->class(),
         ]);
 
-        $featured = $this->featured->artists()->first();
+        $featured->created_at = Carbon::now()->subDays(7);
+        $featured->save();
 
-        $this->assertInstanceOf(
-            $this->profile->class(),
-            $featured->featurable->profile
+        // ... Artist was featured more than 180 days ago (implies longer than
+        // 7 days ago, obviously), which makes the Artist eligible again.
+
+        $artist = $this->createArtist();
+
+        $this->makeAlbum(['artist_id' => $artist->id])->save();
+
+        $featured = $this->featured->create([
+            'featurable_id' => $artist->id,
+            'featurable_type' => $this->artist->class(),
+        ]);
+
+        $featured->created_at = Carbon::now()->subDays(180);
+        $featured->save();
+    }
+
+    private function createArtistAndDeleteItsProfile()
+    {
+        $artist = $this->createArtist();
+
+        $artist->profile()->delete();
+
+        return $artist;
+    }
+
+    public function testFeaturableResultsHasExactlyThreeEntities()
+    {
+        $this->assertEquals(
+            3,
+            $this->artist->featurable()->get()->count()
         );
     }
 
+    /*
+    public function testFeaturedResultsAreExcluded()
+    {
+
+    }
+
+    public function testFeaturedResultsAreEligible()
+    {
+
+    }
+
+    public function testFeaturedEntityInstanceOfArtist()
+    {
+
+    }
+
+    public function testFeaturedArtistExists()
+    {
+
+    }
+    */
+
     /**
-     * Create an Album.
+     * Create an Artist.
+     *
+     * @param array $properties
+     * @return \App\Artist
+     */
+    protected function createArtist(array $properties = [])
+    {
+        return factory($this->artist->class())->create($properties);
+    }
+
+    /**
+     * Make an Album.
      *
      * @param array $properties
      * @return \App\Album
      */
     protected function makeAlbum(array $properties = [])
     {
-        $artist = factory($this->artist->class())->create();
-
         // This is the one property that can't be passed via the argument.
 
-        $properties['artist_id'] = $artist->id;
+        $properties['artist_id'] = $properties['artist_id'] ?? $this->createArtist()->id;
 
         return factory($this->album->class())->make($properties);
     }
