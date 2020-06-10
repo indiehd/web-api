4d960cb0e416a67d1a944f85e9520974d42519f3
Michael Bøcker-Larsen <m19n@pm.me>
2020-06-10 16:38:53 +0800

    test: add more money tests

--- tests/Feature/Casts/MoneyTest.php
+++ tests/Feature/Casts/MoneyTest.php
@@ -31,6 +31,28 @@ class MoneyTest extends TestCase
     }
 
     /** @test */
+    public function testCanSetMoney()
+    {
+        $album = new Album;
+
+        $album->full_album_price = Money::EUR(42);
+
+        $this->assertInstanceOf(Money::class, $album->full_album_price);
+        $this->assertEquals(42, $album->full_album_price->getAmount());
+        $this->assertEquals('EUR', $album->full_album_price->getCurrency()->getCode());
+    }
+
+    /** @test */
+    public function testCanSetNull()
+    {
+        $album = new Album;
+
+        $album->full_album_price = null;
+
+        $this->assertNull($album->full_album_price);
+    }
+
+    /** @test */
     public function testCanStore()
     {
         $album = factory(Album::class)->make(['full_album_price' => 999]);
@@ -49,5 +71,23 @@ class MoneyTest extends TestCase
         $this->assertFalse($price === $album->full_album_price);
         // But same
         $this->assertEquals($price, $album->full_album_price);
+    }
+
+    /** @test */
+    public function testCanModify()
+    {
+        $album = new Album;
+
+        $album->full_album_price = 999;
+
+        /** @var Money */
+        $money = $album->full_album_price;
+
+        $album->full_album_price = $money->add(Money::EUR(600))
+            ->subtract(Money::EUR(100))
+            ->multiply(2)
+            ->divide(2);
+
+        $this->assertEquals(1499, $album->full_album_price->getAmount());
     }
 }
