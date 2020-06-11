8f7c08d86c53aa88f565e2de147c008f265632d5
Michael Bøcker-Larsen <m19n@pm.me>
2020-06-11 13:23:22 +0800

    style: fix indentation issues

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -21,37 +21,37 @@ class LoginController extends Controller
     protected $hasher;
 
     public function __construct(UserRepositoryInterface $user, Hasher $hasher)
-   {
-       $this->user = $user;
-       $this->hasher = $hasher;
-   }
+    {
+        $this->user = $user;
+        $this->hasher = $hasher;
+    }
 
     public function validateUser(Request $request)
-   {
-       $email = $request->get('email');
-       $password = $request->get('password');
+    {
+        $email = $request->get('email');
+        $password = $request->get('password');
 
-       $user = $this->user->model()
-           ->whereEmail($email)
-           ->first();
+        $user = $this->user->model()
+            ->whereEmail($email)
+            ->first();
 
-       if (!$user) {
-           return response('User Not Found', 401);
-       }
+        if (!$user) {
+            return response('User Not Found', 401);
+        }
 
-       if ($this->hasher->check($password, $user->password)) {
-           $token = $user->token();
+        if ($this->hasher->check($password, $user->password)) {
+            $token = $user->token();
 
-           if (!$token) {
-               $token = $user->createToken('Personal Access Token')->accessToken;
-           }
+            if (!$token) {
+                $token = $user->createToken('Personal Access Token')->accessToken;
+            }
 
-           return response([
-               'access_token' => $token,
-               'user' => $user
-           ], 200);
-       }
+            return response([
+                'access_token' => $token,
+                'user' => $user
+            ], 200);
+        }
 
-       return response('Unauthorized', 401);
-   }
+        return response('Unauthorized', 401);
+    }
 }
