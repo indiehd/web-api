3ddf3ac61676da26255a2c954a2789b8aff6629b
Michael Bøcker-Larsen <m19n@pm.me>
2020-06-11 12:55:01 +0800

    test: let ControllerTestCase act as using Passport

--- tests/Feature/Controllers/ControllerTestCase.php
+++ tests/Feature/Controllers/ControllerTestCase.php
@@ -2,11 +2,23 @@
 
 namespace Tests\Feature\Controllers;
 
+use App\Contracts\UserRepositoryInterface;
 use Tests\TestCase;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Passport\Passport;
 
 abstract class ControllerTestCase extends TestCase
 {
     use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $user_repository = resolve(UserRepositoryInterface::class);
+        $user = factory($user_repository->class())->create();
+
+        Passport::actingAs($user);
+    }
 }
