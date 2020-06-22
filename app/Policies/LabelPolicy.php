78457442f4db2f4e0a490d226e5713d7d2f7510d
Michael Bøcker-Larsen <m19n@pm.me>
2020-06-22 11:19:58 +0800

    fix: update write abilities in LabelPolicy

--- app/Policies/LabelPolicy.php
+++ app/Policies/LabelPolicy.php
@@ -53,7 +53,7 @@ class LabelPolicy
      */
     public function update(User $user, Label $label)
     {
-        return $user->is($label->contenable);
+        return $user->is($label->contenable->user);
     }
 
     /**
@@ -65,7 +65,7 @@ class LabelPolicy
      */
     public function delete(User $user, Label $label)
     {
-        return $user->is($label->contenable);
+        return $user->is($label->contenable->user);
     }
 
     /**
@@ -77,7 +77,7 @@ class LabelPolicy
      */
     public function restore(User $user, Label $label)
     {
-        return $user->is($label->contenable);
+        return $user->is($label->contenable->user);
     }
 
     /**
